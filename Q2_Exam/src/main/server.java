f45d529cea4c9649cb2b2e7e24fff2b459f5d613
Antoine Culot <74051477+Antoine-Clt@users.noreply.github.com>
2023-05-10 10:07:53 +0200

    Add files via upload

--- Q2_Exam/src/main/server.java
+++ Q2_Exam/src/main/server.java
@@ -1,20 +1,20 @@
 package main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
 import java.time.*;
-
 import ti208.log.*;
 
 public class server {
-//preparer les statements
+//utilisation de PrepareStatement pour les injections SQL sujettes à répétition (protège aussi des attaques SQL mais ne nous concernent pas)
 //static String command = "CREATE TABLE log (id int PRIMARY KEY, date time, customer_id int, info varchar (200), level varchar(10))";
 
-	public static void main(String args[]) {
-		try(Connection conn = DriverManager.getConnection("jdbc:sqlite:sample.db"); Statement st = conn.createStatement()){
+	public static void main(String args[]) throws IOException {
+		try(Connection conn = DriverManager.getConnection("jdbc:sqlite:sample.db"); 
+			Statement st = conn.createStatement();)
+		
+		{
 			// Use connection
 			System.out.println("Connexion réussie !");
 			
@@ -27,20 +27,29 @@ public class server {
 			LocalDateTime timeInput = log.getDatetime();
 			LogLevel levelInput = log.getLevel();
 			String textInput = log.getText();
-			String insertCommand = "INSERT INTO log (id, time, text, level) VALUES (1, '" + timeInput.toString() + "', '" + textInput + "', '" + levelInput.toString() + "')";
+			String insertCommand = "INSERT INTO log (id, time, text, level) VALUES (2, '" + timeInput + "', '" + textInput + "', '" + levelInput + "')";
 			st.executeUpdate(insertCommand);
 			
 			//Récupération de données
 			String resultCommand = "Select * FROM log";
 			ResultSet result = st.executeQuery(resultCommand);
 			
-			//Affichage des résultats
 			while(result.next()) {
 				int id = result.getInt("id");
 				String timeOutput = result.getString("time");
 				String textOutput = result.getString("text");
 				String levelOutput = result.getString("level");
-				System.out.println("| id: " + id + " | time: " + timeOutput + " | text: " + textOutput + " | level: " + levelOutput + " |");
+				
+				String output = "| id: " + id + " | time: " + timeOutput + " | text: " + textOutput + " | level: " + levelOutput + " |";
+				//affichage du log dans le terminal
+				System.out.println(output);
+				
+				//écriture du log dans le fichier texte
+				FileWriter fw = new FileWriter("BD.txt", true);
+				fw.write("\n" + output);
+				fw.close();
+				
+				
 				}
 			
 			}
