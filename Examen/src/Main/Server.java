bc7bf694e3aa11a11904a832599d661f95da04a0
BlueConor <102811109+BlueConor@users.noreply.github.com>
2023-05-10 08:43:30 +0200

    Transfert de données Cl/srv fonctionnel

--- Examen/src/Main/Server.java
+++ Examen/src/Main/Server.java
@@ -3,22 +3,27 @@ package Main;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import ti208.log.*;
 
 public class Server {
 	public static void main(String[] args) {
 		
 		ServerSocket server;
-		Socket socket;
+		Socket connection;
 		ObjectInputStream input;
 		
 		
 		try {
 			server = new ServerSocket(5678);
-			socket = server.accept();
+			connection = server.accept();
 			
-			input = new ObjectInputStream(socket.getInputStream());
-			System.out.println(input.readObject());
+			input = new ObjectInputStream(connection.getInputStream());
 			
+			while(true) {
+				String id = (String) input.readObject();
+				Log message = (Log) input.readObject();
+				System.out.println(id + message);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
