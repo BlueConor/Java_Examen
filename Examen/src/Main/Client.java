5ebfc79f372099d8b20b1c819f6dda5bf73524c5
BlueConor <102811109+BlueConor@users.noreply.github.com>
2023-05-10 15:37:52 +0200

    Ajout commentaire  + modifs mineurs

--- Examen/src/Main/Client.java
+++ Examen/src/Main/Client.java
@@ -8,26 +8,36 @@ import ti208.log.*;
 public class Client {
 	public static void main(String[] args) {
 		
+		//Vérification de la présence de l'argument/ID du client
 		if(args.length == 0) {
 			System.out.println("Vous avez oublé de donnée un ID à votre client");
 			System.exit(1);
 		}
 		
-		final String id = args[0];
-		final Socket server;
-		final ObjectOutputStream output;
-		
+		//Transmission des logs vers le serveur depuis le client
 		try {
-			server = new Socket("localhost", 5678);
-			output = new ObjectOutputStream(server.getOutputStream());
 			
+			//Création des systèmes de communation au serveur
+			Socket server = new Socket("localhost", 5678);
+			ObjectOutputStream output = new ObjectOutputStream(server.getOutputStream());
+			
+			//Boucle pour la transmission des logs du clients
 			for(int i = 0; i < 5; i++) {
 				output.writeObject(LogGenerator.readLog());
-				output.writeObject(id);
+				output.writeObject(args[0]);
 			}
 			
-			output.close();
-			server.close();
+			//Fermeture des systèmes de connections
+			try {
+				if(server != null) {
+					server.close();
+				}
+				if(output != null) {
+					output.close();
+				}
+			} catch (Exception e) {
+				System.out.println("Une erreur s'est produite lors de la fermeture des systèmes de communications avec le serveur.");
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
