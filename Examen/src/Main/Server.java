ac265f6cb87886dccef55dc8ae5a6986c128e4be
BlueConor <jerome26.francois@outlook.be>
2023-05-28 12:50:08 +0200

    mise à jour

--- Examen/src/Main/Server.java
+++ Examen/src/Main/Server.java
@@ -1,45 +1,23 @@
-package Main;
+package main;
 
-import java.io.ObjectInputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import ti208.log.*;
 
 public class Server {
-	public static void main(String[] args) {
+
+	public static void main(String args[]) throws IOException{
 		
-		try {
+		try (ServerSocket server = new ServerSocket(5678)) {
+			Thread menu = new Interface();
+			menu.start();
 			
-			//Création du serveur et de l'attente de connection cliente
-			ServerSocket server = new ServerSocket(5678);
-			Socket connection = server.accept();
-			
-			ObjectInputStream input = new ObjectInputStream(connection.getInputStream());
-			
-			//Boucle pour récuperer les logs et l'identifiant du client, ainsi que les afficher
-			for(int i = 0; i < 5; i++){
-				Log message = (Log) input.readObject();
-				String id = (String) input.readObject();
-				System.out.println("'Le client' " + id + " a envoyé le log suivant :\n" + message);
+			while(true) {
+				Socket connection = server.accept();
 				
+				Threading thread = new Threading(connection);
+				thread.start();
 			}
-			
-			try {
-				if(server != null) {
-					server.close();
-				} 
-				if(connection != null) {
-					connection.close();
-				}
-				if(input != null) {
-					input.close();
-				}
-			} catch (Exception e) {
-				System.out.println("Une erreur s'est produite lors de la fermeture des systèmes de communications avec le client.");
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
 	}
 }
